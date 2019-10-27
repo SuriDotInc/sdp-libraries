ed42da76402a3c994d6e24a0e7b28936d03fea3b
Surender Yeruva <40207902+SuriDotInc@users.noreply.github.com>
2019-10-27 09:35:40 -0400

    Update static_code_analysis.groovy

--- sonarqube/static_code_analysis.groovy
+++ sonarqube/static_code_analysis.groovy
@@ -36,10 +36,10 @@ def call(){
           if (!fileExists("sonar-project.properties"))
             script += "-Dsonar.sources=\"./src\""
           sh "ls -lartR /var/lib/jenkins/workspace/JenkinsDemo_master@tmp"
-          sh "cat /var/lib/jenkins/workspace/JenkinsDemo_master@tmp/durable*/script.sh"
+          sh "cat /var/lib/jenkins/workspace/JenkinsDemo_master@tmp/*/script.sh"
           sh "id"
           sh "cat /var/lib/jenkins/workspace/JenkinsDemo_master@tmp/*/config.json"
-          sh "chmod -R ugo+x /var/lib/jenkins/workspace/JenkinsDemo_master@tmp"
+          sh "chmod -R ugo+x /var/lib/jenkins/workspace/JenkinsDemo_master@tmp/*/script.sh"
           sh "ls -lartR /var/lib/jenkins/workspace/JenkinsDemo_master@tmp"
           sh script
             
